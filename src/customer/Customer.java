e5e5d29f314de9b064f5efb3ef4b618a586a7cdf
Padulu <patrick-kaufmann@hotmail.com>
2015-06-30 11:28:32 +0200

    bla

--- src/customer/Customer.java
+++ src/customer/Customer.java
@@ -5,6 +5,9 @@ import javax.faces.bean.SessionScoped;
 
 //import com.sun.jndi.cosnaming.IiopUrl.Address;
 
+
+
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,9 +15,12 @@ import java.util.List;
 @ManagedBean
 @SessionScoped
 public class Customer {
+	private String name;
+	private String surname;
+	
 	private List<Address> addresses = new ArrayList<Address>(Arrays.asList(
-			new Address(8052, "Zürich", "Federnstrasse", "Fredi", "Krueger"),
-			new Address(3997, "Bellwald", "Imwinkelried", "Milla", "Fair")
+			new Address(8052, "Zürich", "Federnstrasse"),
+			new Address(3997, "Bellwald", "Imwinkelried")
 		));
 	
 	public List<Address> getAddresses(){
@@ -23,5 +29,18 @@ public class Customer {
 	
 	public void setAddresses(List<Address> addresses){
 		this.addresses = addresses;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getSurname(){
+		return surname;
+	}
+	public void setZipCode(String surname){
+		this.surname = surname;
 	}
 }
